c24298c9c7b023670138c4980bd814dc76de5c02
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-08-02 10:13:02 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ scripts = ['data-viewer']
 
 setup(
     name='data_viewer',
-    version='1.4.0',
+    version='1.5.0',
 
     description=short_description,
     long_description=long_description,
